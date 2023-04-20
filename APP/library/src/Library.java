f752039f2545378164bf697f8055c3370fac4ebc
Puszmen <piotr.zatorski91@gmail.com>
2023-04-20 20:33:01 +0200

    first commit with my APP

---
+++ APP/library/src/Library.java
@@ -0,0 +1,24 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Library v0.1";
+
+        String title = "Lord of The Rings";
+        String author = "J.R.R. Tolkien";
+        int relaseDate = 1955;
+        int pages = 1500;
+        String publisher = "Peter";
+        String isbn = "326598784512";
+
+        System.out.println(appName);
+        System.out.println("Books available in the library");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(relaseDate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+
+
+
+    }
+}
