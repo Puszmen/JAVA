f9ca846d644e4f916ce9c145aaebbcf9d944573a
Puszmen <piotr.zatorski91@gmail.com>
2023-05-08 19:45:40 +0200

    created program *TestDice* about using overloaded constructors

---
+++ 8. Methods overloads and word this/test2/src/TestDice.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+class TestDice {
+    public static void main(String[] args) {
+        Dice dice = new Dice();
+        dice.printInfo();
+        dice.roll();
+        dice.printInfo();
+        dice.roll();
+        dice.printInfo();
+    }
+}
