a4f9888a3952a2a943628d0c983624c86b2313d3
Puszmen <piotr.zatorski91@gmail.com>
2023-05-25 17:46:10 +0200

    app updated to v0.7, created array([1000]) to put there new books

--- APP/library/src/pl/javastart/library/app/Library.java
+++ APP/library/src/pl/javastart/library/app/Library.java
@@ -4,19 +4,20 @@ import pl.javastart.library.model.Book;
 
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.6";
+        final String appName = "Biblioteka v0.7";
 
-        Book book1 = new Book("Lord of The Rings","J.R.R. Tolkien",1995,1500,
+        Book[] books = new Book[1000];
+        books[0] = new Book("Lord of The Rings","J.R.R. Tolkien",1995,1500,
                 "Peter","326-59-8784-512-0");
-        Book book2 = new Book("Milczenie owiec","Thomas Harris",2015,432,
+        books[1] = new Book("Milczenie owiec","Thomas Harris",2015,432,
                 "Andre","978-83-7985-693-0");
-        Book book3 = new Book("Żywot Nunczako","Mariano Italiano",2018,100,
+        books[2] = new Book("Żywot Nunczako","Mariano Italiano",2018,100,
                 "Pietraszek");
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece");
-        book1.printInfo();
-        book2.printInfo();
-        book3.printInfo();
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
     }
 }
