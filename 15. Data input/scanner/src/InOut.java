debe32348a0761c015bde84e4a734476b810ada4
Puszmen <piotr.zatorski91@gmail.com>
2023-05-29 14:53:13 +0200

    creted new file Data input and added basic programs InOut and TwoNumbers about using scanner class

---
+++ 15. Data input/scanner/src/InOut.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class InOut {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wprowadź swój wiek:");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Wprowadź swoje imię:");
+        String firstName = scanner.nextLine();
+        System.out.println("Wprowadź swoje nazwisko:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Cześć "+firstName+" "+lastName+" !");
+        System.out.println("Masz już "+age+" lat, starość nie radość :(");
+    }
+}
---
+++ 15. Data input/scanner/src/TwoNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        System.out.println("Podaj A:");
+        double a = scanner.nextDouble();
+        System.out.println("Podaj B:");
+        double b = scanner.nextDouble();
+
+        scanner.close();
+        System.out.println("A + B = "+(a + b));
+    }
+}
