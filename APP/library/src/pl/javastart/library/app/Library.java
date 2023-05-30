5587de2869b960ca6fff65ff4871700d60df81cb
Puszmen <piotr.zatorski91@gmail.com>
2023-05-30 13:41:38 +0200

    app updated to v0.8, added new class DataReader with method to create new books using Scanner class

--- APP/library/src/pl/javastart/library/app/Library.java
+++ APP/library/src/pl/javastart/library/app/Library.java
@@ -1,23 +1,21 @@
 package pl.javastart.library.app;
 
+import pl.javastart.library.io.DataReader;
 import pl.javastart.library.model.Book;
+
+import java.util.Scanner;
 
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.7";
+        final String appName = "Biblioteka v0.8";
 
         Book[] books = new Book[1000];
-        books[0] = new Book("Lord of The Rings","J.R.R. Tolkien",1995,1500,
-                "Peter","326-59-8784-512-0");
-        books[1] = new Book("Milczenie owiec","Thomas Harris",2015,432,
-                "Andre","978-83-7985-693-0");
-        books[2] = new Book("Żywot Nunczako","Mariano Italiano",2018,100,
-                "Pietraszek");
+        DataReader dataReader = new DataReader();
 
         System.out.println(appName);
-        System.out.println("Książki dostępne w bibliotece");
+        System.out.println("Proszę wprowadzić książkę do systemu");
+        books[0] = dataReader.readAndCreateBook();
+        dataReader.close();
         books[0].printInfo();
-        books[1].printInfo();
-        books[2].printInfo();
     }
 }
