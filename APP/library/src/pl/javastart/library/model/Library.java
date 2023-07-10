9b98792b004074774430dfa6173c907e61b4c845
Puszmen <piotr.zatorski91@gmail.com>
2023-07-10 19:33:12 +0200

    updated app to v1.2, used polymorphism to change Library class, also added method printInfo in Publication class to use polymorphism in this method

--- APP/library/src/pl/javastart/library/model/Library.java
+++ APP/library/src/pl/javastart/library/model/Library.java
@@ -2,45 +2,51 @@ package pl.javastart.library.model;
 
 public class Library {
 
-    private static final int MAX_BOOKS = 1000;
-    private static final int MAX_MAGAZINES = 1000;
-    private Book[] books = new Book[MAX_BOOKS];
-    private Magazine[] magazines = new Magazine[MAX_MAGAZINES];
-    private int booksNumber = 0;
-    private int magazinesNumbers = 0;
+    private static final int MAX_PUBLICATIONS = 2000;
+    private int publicationsNumber = 0;
+    private Publication[] publications = new Publication[MAX_PUBLICATIONS];
+
 
     public void addBook (Book book) {
-        if (booksNumber < MAX_BOOKS) {
-            books[booksNumber] = book;
-            booksNumber++;
+        if (publicationsNumber < MAX_PUBLICATIONS) {
+            publications[publicationsNumber] = book;
+            publicationsNumber++;
         } else {
             System.out.println("Osiągnięto maksymalną liczbę książek w bibliotece");
         }
     }
-    public void printBooks () {
-        if (booksNumber == 0) {
-            System.out.println("Brak książek w bibliotece");
+    public void printBooks() {
+        int countBooks = 0;
+        for (int i = 0; i < publicationsNumber; i++) {
+            if (publications[i] instanceof Book) {
+                publications[i].printInfo();
+                countBooks++;
+            }
         }
-        for (int i = 0; i < booksNumber; i++) {
-            books[i].printInfo();
+        if (countBooks == 0) {
+            System.out.println("Brak książek w bibliotece");
         }
     }
 
     public void addMagazine (Magazine magazine) {
-        if (magazinesNumbers < MAX_MAGAZINES) {
-            magazines[magazinesNumbers] = magazine;
-            magazinesNumbers++;
+        if (publicationsNumber < MAX_PUBLICATIONS) {
+            publications[publicationsNumber] = magazine;
+            publicationsNumber++;
         } else {
             System.out.println("Osiągnięto maksymalną liczbę magazynów w bibliotece");
         }
     }
 
-    public void printMagazines () {
-        if (magazinesNumbers == 0) {
-            System.out.println("Brak magazynów w bibliotece");
+    public void printMagazines() {
+        int countMagazines = 0;
+        for (int i = 0; i < publicationsNumber; i++) {
+            if (publications[i] instanceof Magazine) {
+                publications[i].printInfo();
+                countMagazines++;
+            }
         }
-        for (int i = 0; i < magazinesNumbers; i++) {
-            magazines[i].printInfo();
+        if (countMagazines == 0) {
+            System.out.println("Brak magazynów w bibliotece");
         }
     }
 }
--- APP/library/src/pl/javastart/library/model/Publication.java
+++ APP/library/src/pl/javastart/library/model/Publication.java
@@ -35,4 +35,9 @@ public class Publication {
     public void setPublisher(String publisher) {
         this.publisher = publisher;
     }
+
+    public void printInfo () {
+    }
+
+
 }
