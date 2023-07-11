b28e6482d8368b45cfedcec86af5cdfd530ed76a
Puszmen <piotr.zatorski91@gmail.com>
2023-07-11 19:18:06 +0200

    in class Student created overrided method toString and in class School showed how to use toString in code

--- 23. Object class methods/object_equals/src/School.java
+++ 23. Object class methods/object_equals/src/School.java
@@ -2,8 +2,8 @@ public class School {
     public static void main(String[] args) {
 
         Student student1 = new Student("Jan Kowalski", 25);
-        Student student2 = new Student("Jan Kowalski", 25);
-        Student student3 = new Student("Kinga Bartnik", 19);
+//        Student student2 = new Student("Jan Kowalski", 25);
+//        Student student3 = new Student("Kinga Bartnik", 19);
 
 //        System.out.println("student1 == student2");
 //        System.out.println(student1 == student2);
@@ -11,10 +11,15 @@ public class School {
 //        boolean equalsStudent = student1.equals(student2);
 //        System.out.println(equalsStudent);
 
-        System.out.println(student1.equals(student2));
-        System.out.println(student1.hashCode());
-        System.out.println(student2.hashCode());
-        System.out.println(student3.hashCode());
+//        System.out.println(student1.equals(student2));
+//        System.out.println(student1.hashCode());
+//        System.out.println(student2.hashCode());
+//        System.out.println(student3.hashCode());
+
+        System.out.println("Lista studentów:");
+        String info = student1.toString();
+        System.out.println(info);
+        System.out.println(student1);
 
     }
 }
--- 23. Object class methods/object_equals/src/Student.java
+++ 23. Object class methods/object_equals/src/Student.java
@@ -27,6 +27,11 @@ public class Student {
     }
 
     @Override
+    public String toString() {
+        return name + " " + age;
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
