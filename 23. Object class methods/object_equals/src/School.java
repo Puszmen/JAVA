4fca22f011f9caa386b1a59d387b44201020691b
Puszmen <piotr.zatorski91@gmail.com>
2023-07-11 18:59:16 +0200

    in class School showed how is working method hashCode (its related with method equals - always together)

--- 23. Object class methods/object_equals/src/School.java
+++ 23. Object class methods/object_equals/src/School.java
@@ -3,11 +3,18 @@ public class School {
 
         Student student1 = new Student("Jan Kowalski", 25);
         Student student2 = new Student("Jan Kowalski", 25);
+        Student student3 = new Student("Kinga Bartnik", 19);
 
-        System.out.println("student1 == student2");
-        System.out.println(student1 == student2);
-        System.out.println("student1.equals(student2)");
-        boolean equalsStudent = student1.equals(student2);
-        System.out.println(equalsStudent);
+//        System.out.println("student1 == student2");
+//        System.out.println(student1 == student2);
+//        System.out.println("student1.equals(student2)");
+//        boolean equalsStudent = student1.equals(student2);
+//        System.out.println(equalsStudent);
+
+        System.out.println(student1.equals(student2));
+        System.out.println(student1.hashCode());
+        System.out.println(student2.hashCode());
+        System.out.println(student3.hashCode());
+
     }
 }
