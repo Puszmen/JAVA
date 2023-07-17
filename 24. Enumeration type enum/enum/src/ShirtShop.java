c8eb3f6917671d594d3a298070af6bd831f4f826
Puszmen <piotr.zatorski91@gmail.com>
2023-07-17 21:18:57 +0200

    in program ShirtShop showed how to use from Enum class methods values(), name() or ordinar()

--- 24. Enumeration type enum/enum/src/ShirtShop.java
+++ 24. Enumeration type enum/enum/src/ShirtShop.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ShirtShop {
 
     public static void main(String[] args) {
@@ -7,12 +9,27 @@ public class ShirtShop {
 //        System.out.println("MEDIUM - " + Tshirt.MEDIUM);
 //        System.out.println("LARGE - " + Tshirt.LARGE);
 
-        System.out.println(Size.SMALL.getDescription());
-        System.out.println(Size.MEDIUM.getDescription());
-        System.out.println(Size.LARGE.getDescription());
+//        System.out.println(Size.SMALL.getDescription() + " " + Size.SMALL.name() + " " + Size.SMALL.ordinal());
+//        System.out.println(Size.MEDIUM.getDescription());
+//        System.out.println(Size.LARGE.getDescription());
+
+        Size[] values = Size.values();
+        System.out.println("Dostępne rozmiary T-shirtów");
+        for (Size value : values) {
+            System.out.println(value.toString());
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj rozmiar");
+
+        String size = sc.nextLine();
+        Size shortSize = Size.fromDescription(size);
+
 
         Tshirt tshirt = new Tshirt();
-        tshirt.setSize(Size.SMALL);
+        tshirt.setSize(shortSize);
+        System.out.println(tshirt.getSize());
+        System.out.println("Rozmiar T-shirta: " + tshirt.getSize().getDescription());
 
     }
 }
--- 24. Enumeration type enum/enum/src/Size.java
+++ 24. Enumeration type enum/enum/src/Size.java
@@ -11,4 +11,19 @@ public enum Size {
     public String getDescription() {
         return description;
     }
+
+    public static Size fromDescription (String description) {
+        Size[] values = values();
+        for (Size size : values) {
+            if (size.getDescription().equals(description)) {
+                return size;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return description + " " + name() + " " + ordinal();
+    }
 }
