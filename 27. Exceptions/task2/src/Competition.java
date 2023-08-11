85ef79b19ed878973bca1e94bdb6607a6f959763
Puszmen <piotr.zatorski91@gmail.com>
2023-08-11 13:44:39 +0200

    added task2 and CompetitionApp to show how do use exceptions in the app to avoid problems (negative naumbers, letters instead of letter and vice versa etc)

---
+++ 27. Exceptions/task2/src/Competition.java
@@ -0,0 +1,38 @@
+class Competition {
+    private String name;
+    private Participant[] participants;
+    private int ageLimit;
+    private int size;
+
+    public Competition(String name, int maxParticipants, int ageLimit) {
+        this.name = name;
+        if (ageLimit <= 0)
+            throw new IllegalArgumentException ("ageLimit has to be positive number");
+        this.ageLimit = ageLimit;
+        this.participants = new Participant[maxParticipants];
+    }
+
+    void addParticipant(Participant participant) {
+        participants[size] = participant;
+        size++;
+    }
+
+    boolean hasFreeSpots() {
+        return size < participants.length;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Zawody ")
+                .append(name)
+                .append("\n")
+                .append("Liczba uczestników: ").append(size);
+        for (int i = 0; i < size; i++) {
+            builder.append("\n");
+            builder.append(" > ");
+            builder.append(participants[i].toString());
+        }
+        return builder.toString();
+    }
+}
