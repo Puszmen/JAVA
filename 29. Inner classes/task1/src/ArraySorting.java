f0dac1a61a5a37bffcca7fbda33993ed6b0383a5
Puszmen <piotr.zatorski91@gmail.com>
2023-08-26 16:35:02 +0200

    added program about sorting int values in the array. Used interface as a inner class with method where is keeped array

---
+++ 29. Inner classes/task1/src/ArraySorting.java
@@ -0,0 +1,35 @@
+public class ArraySorting {
+    public static void main(String[] args) {
+
+        Sortable sortable = new Sortable() {
+            @Override
+            public void sort(int[] array) {
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 1; j < array.length - i; j++) {
+                        if (array[j - 1] > array[j]) {
+                            int temp = array[j];
+                            array[j] = array[j - 1];
+                            array[j - 1] = temp;
+                        }
+                    }
+                }
+            }
+        };
+        int[] tab = {1,3,9,5,2,8,7,6,4,10};
+
+        printArray(tab);
+        sortable.sort(tab);
+        printArray(tab);
+    }
+
+    private static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    interface Sortable {
+        void sort (int[] tab);
+    }
+}
