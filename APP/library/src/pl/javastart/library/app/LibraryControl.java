4d107e7ae8a51259166a42eee70d0b4a63de8445
Puszmen <piotr.zatorski91@gmail.com>
2023-08-26 16:43:28 +0200

    app updated to v1.6, class Option has benn removed and code form this class moved to class LibraryControl as a inner class(private enum Option)

--- APP/library/src/pl/javastart/library/app/LibraryControl.java
+++ APP/library/src/pl/javastart/library/app/LibraryControl.java
@@ -103,4 +103,42 @@ public class LibraryControl {
             printer.printLine(option.toString());
         }
     }
+
+    private enum Option {
+
+        EXIT(0, "wyjście z programu"),
+        ADD_BOOKS(1, "dodaj książkę"),
+        ADD_MAGAZINES(2, "dodaj magazyn"),
+        PRINT_BOOKS(3, "wyświetl dostępne książki"),
+        PRINT_MAGAZINES(4, "wyświetl dostępne magazyny");
+
+        private final int value;
+        private final String description;
+
+        Option(int value, String description) {
+            this.value = value;
+            this.description = description;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public String toString() {
+            return value + " - " + description;
+        }
+
+        static Option createFromInt (int option) throws NoSuchOptionException {
+            try {
+                return Option.values()[option];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new NoSuchOptionException("Brak opcji o ID " + option);
+            }
+        }
+    }
 }
