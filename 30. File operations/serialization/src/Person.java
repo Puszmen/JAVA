3782e450844827834178bd459ba5aaf9a8168777
Puszmen <piotr.zatorski91@gmail.com>
2023-08-30 17:14:44 +0200

    added programs about serialization objects (reading and writing objects into created files)

---
+++ 30. File operations/serialization/src/Person.java
@@ -0,0 +1,34 @@
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 3812017177088226528L;
+
+    private String firstName;
+    private String secondName;
+
+    public Person(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+
+}
