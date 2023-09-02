0ea6c1d97b4ed7621016b45a488e39ff6db68b6a
Puszmen <piotr.zatorski91@gmail.com>
2023-09-02 22:05:14 +0200

    app updated to v1.8 added new package classes interface, enum and added new exception classes. Created methods to load and save data to file

--- APP/library/src/pl/javastart/library/app/LibraryControl.java
+++ APP/library/src/pl/javastart/library/app/LibraryControl.java
@@ -1,8 +1,12 @@
 package pl.javastart.library.app;
 
+import pl.javastart.library.exception.DataExportException;
+import pl.javastart.library.exception.DataImportException;
 import pl.javastart.library.exception.NoSuchOptionException;
 import pl.javastart.library.io.ConsolePrinter;
 import pl.javastart.library.io.DataReader;
+import pl.javastart.library.io.file.FileManager;
+import pl.javastart.library.io.file.FileManagerBuilder;
 import pl.javastart.library.model.Book;
 import pl.javastart.library.model.Library;
 import pl.javastart.library.model.Magazine;
@@ -14,9 +18,22 @@ public class LibraryControl {
 
     private ConsolePrinter printer = new ConsolePrinter();
     private DataReader dataReader = new DataReader(printer);
-    private Library library = new Library();
+    private FileManager fileManager;
+    private Library library;
 
-    public void controlLoop () {
+    LibraryControl() {
+        fileManager = new FileManagerBuilder(printer, dataReader).build();
+        try {
+            library = fileManager.importData();
+            printer.printLine("Zaimportowano dane z pliku.");
+        } catch (DataImportException e) {
+            printer.printLine(e.getMessage());
+            printer.printLine("Zainicjowano nową bazę.");
+            library = new Library();
+        }
+    }
+
+    void controlLoop () {
         Option option;
 
         do {
@@ -77,6 +94,12 @@ public class LibraryControl {
     }
 
     private void exit() {
+        try {
+            fileManager.exportData(library);
+            printer.printLine("Eksport danych do pliku zakończony powodzeniem.");
+        } catch (DataExportException e) {
+            printer.printLine(e.getMessage());
+        }
         printer.printLine("Koniec programu ! :(");
         dataReader.close();
     }
---
+++ APP/library/src/pl/javastart/library/exception/DataImportException.java
@@ -0,0 +1,8 @@
+package pl.javastart.library.exception;
+
+public class DataImportException extends RuntimeException {
+
+    public DataImportException(String message) {
+        super(message);
+    }
+}
