9ffefc19129272e0e35161d28b5779bf3d59b57c
Puszmen <piotr.zatorski91@gmail.com>
2023-11-20 19:13:01 +0100

    added task1 about using stream methods (intermediate and terminal) on Integer objects(Stream.iterate)

---
+++ 44. Streams/task1/src/StreamsTask.java
@@ -0,0 +1,14 @@
+import java.util.stream.Stream;
+
+public class StreamsTask {
+    public static void main(String[] args) {
+
+        Stream.iterate(0, x -> x + 1)
+                .filter(x -> x > 100 && x < 1000 && x % 5 == 0)
+//                .filter(x -> x < 1000)
+//                .filter(x -> x % 5 == 0)
+                .limit(10)
+                .map(x -> x * 3)
+                .forEach(System.out::println);
+    }
+}
