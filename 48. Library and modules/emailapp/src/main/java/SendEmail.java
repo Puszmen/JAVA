065bb3679390021dc212fecefbb6f812a4f18941
Puszmen <piotr.zatorski91@gmail.com>
2023-12-15 12:32:14 +0100

    added basic email program, used maven to import exterant libraries (apache commons) from maven repository

--- 48. Library and modules/emailapp/src/main/java/SendEmail.java
+++ 48. Library and modules/emailapp/src/main/java/SendEmail.java
@@ -6,13 +6,13 @@ import org.apache.commons.mail.SimpleEmail;
 class SendEmail {
     public static void main(String[] args) throws EmailException {
         Email email = new SimpleEmail();
-        email.setHostName("poczta.interia.pl");
-        email.setSmtpPort(465);
+        email.setHostName("");
+        email.setSmtpPort(587);
         email.setAuthenticator(new DefaultAuthenticator("", ""));
         email.setSSLOnConnect(true);
         email.setFrom("");
         email.setSubject("Siemaa");
-        email.setMsg("Cześć  :)");
+        email.setMsg("Witam, robię super kurs programowania!");
         email.addTo("");
         System.out.println("Rozpoczęto wysyłanie wiadomości email");
         email.send();
