d7b866bee02f1146a530ca2f4f6e79c5f6fed5fc
Puszmen <piotr.zatorski91@gmail.com>
2023-12-19 19:49:26 +0100

    app updated, added FXMLLoader for creating objects in app based on SceneBuilder(added new dependencies in pom and requirements in module-info)

--- APP MP3/mp3player/src/main/java/pl/javastart/mp3player/main/Main.java
+++ APP MP3/mp3player/src/main/java/pl/javastart/mp3player/main/Main.java
@@ -1,6 +1,9 @@
 package pl.javastart.mp3player.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,7 +12,10 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("MP3 player");
+        Pane mainPane = FXMLLoader.load(getClass().getResource("/fxml/mainPane.fxml"));
+        Scene scene = new Scene(mainPane);
+        stage.setScene(scene);
+        stage.setTitle("mp3 player");
         stage.show();
     }
 }
