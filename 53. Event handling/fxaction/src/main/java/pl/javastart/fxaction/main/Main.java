205e16fd7111a9b7d90a0f9955a853bca063bc55
Puszmen <piotr.zatorski91@gmail.com>
2023-12-29 19:14:23 +0100

    added Mouse and Key event handling (with user)

--- 53. Event handling/fxaction/src/main/java/pl/javastart/fxaction/main/Main.java
+++ 53. Event handling/fxaction/src/main/java/pl/javastart/fxaction/main/Main.java
@@ -1,8 +1,12 @@
 package pl.javastart.fxaction.main;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -14,6 +18,17 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         VBox mainPane = FXMLLoader.load(getClass().getResource("/fxml/mainPane.fxml"));
         Scene scene = new Scene(mainPane);
+
+//        scene.setOnMouseEntered(mouseEvent -> System.out.println("Najechano kursorem na scenę"));
+//        scene.setOnMouseExited(mouseEvent -> System.out.println("Kursor opuścił scenę"));
+
+        scene.addEventFilter(MouseEvent.MOUSE_ENTERED, mouseEvent ->
+                System.out.println("Kursor najechał na scenę"));
+        scene.addEventFilter(MouseEvent.MOUSE_EXITED, mouseEvent ->
+                System.out.println("Kursor opuścił scenę"));
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent ->
+                System.out.println("Klawiatura"));
+
         stage.setScene(scene);
         stage.setTitle("Event example");
         stage.show();
