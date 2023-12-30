99a5ccaf74101c86f17619b7a10e35883941a119
Puszmen <piotr.zatorski91@gmail.com>
2023-12-30 19:40:34 +0100

    app mp3 player updated, added action events to buttons and slider in MainController (setOnAction in lambda expressions)

--- APP MP3/mp3player/src/main/java/pl/javastart/mp3player/controller/MainController.java
+++ APP MP3/mp3player/src/main/java/pl/javastart/mp3player/controller/MainController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
 
@@ -40,7 +41,24 @@ public class MainController {
     private Slider volumeSlider;
 
     public void initialize() {
-        System.out.println("MainController created");
+        configureVolume();
+        configureButtons();
+    }
+
+    private void configureButtons() {
+        nextButton.setOnAction(actionEvent -> System.out.println("Następna piosenka"));
+        previousButton.setOnAction(actionEvent -> System.out.println("Poprzednia piosenka"));
+        playButton.setOnAction(actionEvent -> {
+            if (playButton.isSelected())
+                System.out.println("Play");
+            else
+                System.out.println("Pause");
+        });
+    }
+
+    private void configureVolume() {
+        volumeSlider.addEventFilter(MouseEvent.MOUSE_PRESSED,
+                mouseEvent -> System.out.println("Wciśnięto przycisk głośności"));
     }
 
 }
