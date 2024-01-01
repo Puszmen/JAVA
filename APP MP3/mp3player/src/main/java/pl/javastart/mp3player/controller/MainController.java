4a57d3f83bc01e3a3a76f6287e9375714d8f52ed
Puszmen <piotr.zatorski91@gmail.com>
2024-01-01 18:40:54 +0100

    app mp3 player updated, in mainPane added fx:id(references to all fxml files) to have acces to them. In MainController added variables of provet classes(to have acces)

--- APP MP3/mp3player/src/main/java/pl/javastart/mp3player/controller/MainController.java
+++ APP MP3/mp3player/src/main/java/pl/javastart/mp3player/controller/MainController.java
@@ -1,8 +1,19 @@
 package pl.javastart.mp3player.controller;
 
+import javafx.fxml.FXML;
+
 public class MainController {
+    @FXML
+    private ControlPaneController controlPaneController;
+    @FXML
+    private ContentPaneController contentPaneController;
+    @FXML
+    private MenuPaneController menuPaneController;
 
     public void initialize() {
         System.out.println("Main pane controller created");
+        System.out.println(controlPaneController);
+        System.out.println(contentPaneController);
+        System.out.println(menuPaneController);
     }
 }
